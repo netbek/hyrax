ba9fc7f39b647d808ca4ec4e996fe5b213fe0427
Hein Bekker <hein@netbek.co.za>
2018-05-28 19:29:32 +0200

    Add color and tools to Bokeh scatterplot example, save script to disk

--- app/views/plots_bokeh.py
+++ app/views/plots_bokeh.py
@@ -1,5 +1,9 @@
-from bokeh.models import ColumnDataSource
-from bokeh.plotting import figure, show, output_file, save
+from bokeh.embed import components
+from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.palettes import Category10
+from bokeh.plotting import figure
+from bokeh.transform import factor_cmap
+from lxml import etree
 from vega_datasets import data
 from pyramid.view import view_config
 
@@ -7,6 +11,12 @@ from pyramid.view import view_config
 APP_DIR = 'app'
 PLOTS_DIR = 'app/cache/plots'
 CACHE = False
+
+
+def to_url(filename):
+    if filename.startswith(APP_DIR + '/'):
+        return filename[len(APP_DIR):]
+    return filename
 
 
 class BokehViews:
@@ -19,23 +29,43 @@ class BokehViews:
         """Render the plots"""
         plots = []
 
+        # Build plot
         df = data('iris')
         source = ColumnDataSource(df)
 
-        # colormap = {'setosa': 'red', 'versicolor': 'green', 'virginica': 'blue'}
-        # colors = [colormap[x] for x in flowers['species']]
+        species = df['species'].unique()
+        fill_color = factor_cmap('species', palette=Category10[len(species)], factors=species)
 
-        p = figure(title = "Iris Morphology")
+        hover = HoverTool(tooltips=[
+            ('index', '$index'),
+            ('petalLength', '@petalLength{0.0}'),
+            ('sepalLength', '@sepalLength{0.0}'),
+            ('species', '@species'),
+        ])
+
+        p = figure(title='Iris Morphology', tools=[hover, 'pan', 'wheel_zoom', 'reset'], active_scroll='wheel_zoom')
+
         p.xaxis.axis_label = 'petalLength'
         p.yaxis.axis_label = 'sepalLength'
 
-        p.circle(x='petalLength', y='sepalLength', fill_alpha=0.2, size=10, source=source)
+        p.circle(x='petalLength', y='sepalLength', color=fill_color, fill_alpha=0.5, size=10, source=source)
+
+        # Save plot
+        script, div = components(p)
 
         basename = 'iris_scatter'
-        filename = '{}/{}.html'.format(PLOTS_DIR, basename)
+        filename = '{}/{}.js'.format(PLOTS_DIR, basename)
+        url = to_url(filename)
 
-        # https://bokeh.pydata.org/en/latest/docs/user_guide/embed.html
-        save(p, filename)
+        file = open(filename, 'w')
+        file.write(etree.fromstring(script).text)
+        file.close()
+
+        plots.append({
+            'title': 'Scatterplot',
+            'div': div,
+            'script_url': url
+        })
 
         return {
             'plots': plots
