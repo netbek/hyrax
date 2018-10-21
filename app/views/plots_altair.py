8f2ad6b24c04ce18a750449c64651a6539bec95c
Hein Bekker <hein@netbek.co.za>
2018-10-21 10:51:02 +0200

    Move helper to utils

--- app/views/plots_altair.py
+++ app/views/plots_altair.py
@@ -1,14 +1,10 @@
 import altair as alt
-import inspect
-
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
 
 from vega_datasets import data
 from pyramid.view import view_config
 from .helpers.constants import PNG, SVG
 from .helpers.plots import alt_plot, to_vega
+from .helpers.utils import get_function_source
 
 
 def example_scatterplot():
@@ -27,31 +23,6 @@ def example_scatterplot():
     )
 
     return p
-
-
-def get_function_source(fn):
-    src = inspect.getsource(fn)
-    lines = src.splitlines()
-
-    # Get indentation
-    line = lines[1]
-    indent = len(line) - len(line.lstrip())
-
-    output = []
-    count = len(lines)
-    for i, line in enumerate(lines):
-        # Ignore function declaration and final return statement
-        if i == 0 or i == count - 1:
-            continue
-        # Strip indentation
-        output.append(line[indent:])
-
-    code = '\n'.join(output).strip()
-    lexer = PythonLexer()
-    formatter = HtmlFormatter(noclasses=True)
-    code = highlight(code, lexer, formatter)
-
-    return code
 
 
 @alt_plot
